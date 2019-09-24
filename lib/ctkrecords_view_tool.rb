438129654c19dba2a2e6d7a375f496fc4669eda9
ctkrecords <ctkrecordings@gmail.com>
2019-09-24 11:08:42 -0600

    Dev tool initial commit

---
+++ lib/ctkrecords_view_tool.rb
@@ -0,0 +1,8 @@
+require "ctkrecords_view_tool/version"
+require "ctkrecords_view_tool/renderer"
+
+module CtkrecordsViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
+ 
---
+++ lib/ctkrecords_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CtkrecordsViewTool
+  VERSION = "0.1.0"
+end
